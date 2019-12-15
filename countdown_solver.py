b380d747f8ec354cf34896dfe7d275b84caad285
Michael Blair <michael.blair@live.com.au>
2019-12-15 18:16:35 +1000

    Added iterative version... produces solutions faster and doesn't hit recursion limit. (I need to fix my bad recursive implementation)

--- countdown_solver.py
+++ countdown_solver.py
@@ -15,10 +15,10 @@ def sub(a, b):
     return a - b
 
 ops = {
-    'mult' : mult,
-    'div' : div,
-    'add' : add,
-    'sub' : sub
+    '*' : lambda a, b: a * b,
+    '/' : lambda a, b: a / b,
+    '+' : lambda a, b: a + b,
+    '-' : lambda a, b: a - b
 }
 
 class History:
@@ -30,7 +30,7 @@ class History:
         if val == target:
             self.solved = True
             print('SOLVED!')
-            print(self.ops)
+            print(self.__str__())
         else:
             self.solved = False
         self.current_val = val
@@ -42,19 +42,18 @@ class History:
         new_ops = self.ops + [(self.current_val, op, val, new_val)]
         return History(new_val, self.target, new_ops)
     def __str__(self):
-        return str(self.ops)
-
-def summarise(history):
+        ret_str = ''
+        for op in self.ops:
+            ret_str += str(str(int(op[0])) + ' ' + str(op[1]) + ' ' + str(int(op[2])) + ' = ' + str(int(op[3])) + '\n')
+        return ret_str
+        
 
 
 def solve_numbers(target, numbers=None, histories=None):
     if histories:
         all_solved = True
-
         for i in range(len(histories)):
             if not histories[i][0].solved and len(histories[i][1]) > 0:
-                # print('History: ', histories[i][0])
-                # print('Len outside: ', len(histories[i][1]))
                 all_solved = False
                 new_histories = []
                 hist = histories[i][0]
@@ -62,20 +61,15 @@ def solve_numbers(target, numbers=None, histories=None):
                 for j in range(len(numbers)):
                     outside_numbers = numbers[0:j] + numbers[j+1:]
                     for oper in ops.keys():
-                        # print('Oper: ', oper)
-                        # print('Number: ', numbers[i])
-                        # print('Outside: ', outside_numbers)
                         new_history = hist.op(oper, numbers[j])
                         if new_history != None:
                             new_histories.append((new_history, outside_numbers))
-                
                 histories = histories[0:i] + new_histories + histories[i+1:]
         if all_solved:
             return [history[0] for history in histories if history[0].solved]
         else:
             return solve_numbers(target, histories=histories)
     elif not histories and numbers:
-        # print('Setup called!')
         histories = []
         for i in range(len(numbers)):
             outside_numbers = numbers[0:i] + numbers[i+1:]
@@ -83,8 +77,34 @@ def solve_numbers(target, numbers=None, histories=None):
         histories = solve_numbers(target, histories=histories)
         return histories
 
+
+def solve_iteratively(target, numbers):
+    '''
+    Iteratively append diverging histories (path through all permutations of operators and value orders)
+    to a stack. Returns results much faster than recursive iteration above and doesn't hit any limits.
+    '''
+    solutions = []
+    histories = []
+    ## Setup History Stack
+    for i in range(len(numbers)):
+        histories.append((History(numbers[i], target), numbers[0:i] + numbers[i+1:]))
+    while histories:
+        history_tup = histories.pop()
+        history = history_tup[0]
+        numbers = history_tup[1]
+        for i in range(len(numbers)):
+            for op in ops.keys():
+                new_history = history.op(op, numbers[i])
+                if new_history:
+                    if new_history.solved:
+                        solutions.append(new_history)
+                    else:
+                        histories.append((new_history, numbers[0:i] + numbers[i+1:]))
+    return solutions
+
 if __name__ == '__main__':
     in_targ = 265
     in_nums = [5, 2, 10, 100, 50, 8]
-    solutions = solve_numbers(in_targ, numbers=in_nums)
+    # solutions = solve_numbers(in_targ, numbers=in_nums)
+    solution  = solve_iteratively(in_targ, in_nums)
     code.interact(local=locals())
