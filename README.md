1702d3e9d0ae9881c67505bba7f4dee254f50854
Michael Blair <michael.blair@live.com.au>
2019-12-17 00:42:16 +1000

    Moved recursive to the graveyard.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # countdown_numbers_solver
+
+Solver for the numbers round of the panel show from Countdown (8 Out Of 10 Cats Countdown, of course).
--- countdown_solver.py
+++ countdown_solver.py
@@ -1,5 +1,6 @@
 import code
 import sys
+import argparse
 sys.setrecursionlimit(1500)
 
 ops = {
@@ -23,12 +24,14 @@ class History:
             self.solved = False
         self.current_val = val
         self.target = target
+
     def op(self, op, val):
         new_val = ops[op](self.current_val, val)
         if (not type(new_val) == int and not float.is_integer(new_val)) or new_val < 0:
             return None
         new_ops = self.ops + [(self.current_val, op, val, new_val)]
         return History(new_val, self.target, new_ops)
+
     def __str__(self):
         ret_str = ''
         for op in self.ops:
@@ -38,45 +41,13 @@ class History:
                         + str(int(op[3])) + '\n')
         return ret_str
         
-
-
-def solve_numbers(target, numbers=None, histories=None):
-    if histories:
-        all_solved = True
-        for i in range(len(histories)):
-            if not histories[i][0].solved and len(histories[i][1]) > 0:
-                all_solved = False
-                new_histories = []
-                hist = histories[i][0]
-                numbers = histories[i][1]
-                for j in range(len(numbers)):
-                    outside_numbers = numbers[0:j] + numbers[j+1:]
-                    for oper in ops.keys():
-                        new_history = hist.op(oper, numbers[j])
-                        if new_history != None:
-                            new_histories.append((new_history, outside_numbers))
-                histories = histories[0:i] + new_histories + histories[i+1:]
-        if all_solved:
-            return [history[0] for history in histories if history[0].solved]
-        else:
-            return solve_numbers(target, histories=histories)
-    elif not histories and numbers:
-        histories = []
-        for i in range(len(numbers)):
-            outside_numbers = numbers[0:i] + numbers[i+1:]
-            histories.append((History(numbers[i], target), outside_numbers))
-        histories = solve_numbers(target, histories=histories)
-        return histories
-
-
 def solve_iteratively(target, numbers):
     '''
     Iteratively append diverging histories (path through all permutations of operators and value orders)
-    to a stack. Returns results much faster than recursive iteration above and doesn't hit any limits.
+    to a stack. Returns results much faster than recursive iteration below and doesn't hit any limits.
     '''
     solutions = []
     histories = []
-    ## Setup History Stack
     for i in range(len(numbers)):
         histories.append((History(numbers[i], target), numbers[0:i] + numbers[i+1:]))
     while histories:
@@ -94,9 +65,6 @@ def solve_iteratively(target, numbers):
     return solutions
 
 if __name__ == '__main__':
-    print(sys.argv[1])
-    import argparse
-
     parser = argparse.ArgumentParser()
     parser.add_argument('--numbers',
                         dest='numbers',
@@ -113,3 +81,36 @@ if __name__ == '__main__':
             code.interact(local=locals())
     except:
         pass
+
+########################
+#### CODE GRAVEYARD ####
+########################
+
+# # This was the first attempt, and it's not very efficient =/
+# def solve_recursively(target, numbers=None, histories=None):
+#     if histories:
+#         all_solved = True
+#         for i in range(len(histories)):
+#             if not histories[i][0].solved and len(histories[i][1]) > 0:
+#                 all_solved = False
+#                 new_histories = []
+#                 hist = histories[i][0]
+#                 numbers = histories[i][1]
+#                 for j in range(len(numbers)):
+#                     outside_numbers = numbers[0:j] + numbers[j+1:]
+#                     for oper in ops.keys():
+#                         new_history = hist.op(oper, numbers[j])
+#                         if new_history != None:
+#                             new_histories.append((new_history, outside_numbers))
+#                 histories = histories[0:i] + new_histories + histories[i+1:]
+#         if all_solved:
+#             return [history[0] for history in histories if history[0].solved]
+#         else:
+#             return solve_recursively(target, histories=histories)
+#     elif not histories and numbers:
+#         histories = []
+#         for i in range(len(numbers)):
+#             outside_numbers = numbers[0:i] + numbers[i+1:]
+#             histories.append((History(numbers[i], target), outside_numbers))
+#         histories = solve_recursively(target, histories=histories)
+#         return histories
